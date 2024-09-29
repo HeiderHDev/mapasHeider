87eb1b461f79d101cab20f4cbc5ebca1caed32bc
HeiderHDev <heiderhrnndz@gmail.com>
2024-09-29 16:03:51 -0500

    feat: 🎸 update angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapsModule } from './maps/maps.module';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MapsModule,
-    HttpClientModule
-
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        MapsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
